abb22ce32c78b6020f1ce6f0729f5400fb94005d
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-06-26 23:23:50 +0300

    Correct typo errors and bugs

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 // Routes
 app.use('/api/auth', authRoutes)
-app.use('/api/post', postRoutes)
+app.use('/api/posts', postRoutes)
 
 app.get('/', (req, res) => {
     res.send('API is running...')
