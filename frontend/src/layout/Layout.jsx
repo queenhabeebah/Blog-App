0da3a0e5d1731f7fd121f0a425a48fe276fed250
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-07-08 16:29:24 +0300

    Create and export Layout

---
+++ frontend/src/layout/Layout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const Layout = () => {
+    return(
+        <>
+        <Navbar />
+        <main className="min-h-screen p-4 bg-gray-50">
+            <Outlet />
+        </main>
+        <Footer />
+        </>
+    )
+}
+
+export default Layout
