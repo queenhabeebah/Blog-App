d646e091111698e80b25a12398ad38909a9dd2a6
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-07-08 15:28:23 +0300

    Add Home Page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+        <div className="text-3xl font-bold text-center text-blue-600 mt-10">
+            Welcome to the Blog Home Page
+        </div>
+    )
+}
+
+export default Home
