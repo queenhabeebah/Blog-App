7311f6c024e24aa02f71d6673f6c2b92a294512b
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-07-08 15:39:42 +0300

    Add Dashboard Page

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return(
+        <div className="text-2xl text-center mt-10">
+            Dashboard Page (protected content)
+        </div>
+    )
+}
+
+export default Dashboard
