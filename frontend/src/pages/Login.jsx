f1f5a5cae4c15950f8e5b7ea3661621fbf8891e8
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-07-08 15:34:21 +0300

    Add Login and Register Pages

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div className="text-2xl text-center mt-10">
+      Login Page (form coming soon)
+    </div>
+  );
+};
+
+export default Login
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+    return(
+        <div className="text-2xl text-center mt-10">
+            Register Page (form coming soon)
+        </div>
+    )
+}
+
+export default Register
