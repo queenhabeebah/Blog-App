8dcbdb62d98cd060fa4db39d891a32654f8126e5
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-07-08 19:09:00 +0300

    Import PrivateRoute to App.jsx and wrap Dashboard in it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Layout from "./layout/Layout";
+import PrivateRoute from "./components/PrivateRoutes";
 
 function App() {
   return (
@@ -12,7 +13,12 @@ function App() {
         <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
-        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="dashboard" element={
+          <PrivateRoute>
+            <Dashboard />
+
+          </PrivateRoute>
+          } />
       </Route>
     </Routes>
   );
