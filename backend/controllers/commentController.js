7c07bb1aea38fcfebcd846550e7c5940d70b5993
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-07-19 23:07:33 +0300

    Create GET comments routes in backend

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -1,8 +1,19 @@
 const Comment = require("../models/Comment");
 
+exports.getComments = async (req, res) => {
+  try {
+    const comments = await Comment.find({ post: req.params.postId })
+      .populate("user", "name")
+      .sort({ createAt: 1 });
+    res.json(comments);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to load comments" });
+  }
+};
+
 exports.createComment = async (req, res) => {
   const { text } = req.body;
-  const postId = req.params.postId
+  const postId = req.params.postId;
 
   try {
     const newComment = await Comment.create({
@@ -27,7 +38,7 @@ exports.updateComment = async (req, res) => {
         .status(401)
         .json({ message: "Not authorized to edit this comment" });
     }
-    comment.comment = req.body.comment || comment.comment
+    comment.comment = req.body.comment || comment.comment;
     const updatedComment = await comment.save();
     res.json(updatedComment);
   } catch (error) {
@@ -38,16 +49,21 @@ exports.updateComment = async (req, res) => {
 
 exports.deleteComment = async (req, res) => {
   try {
-    const comment = await Comment.findById(req.params.id).populate('post');
+    const comment = await Comment.findById(req.params.id).populate("post");
     if (!comment) return res.status(404).json({ message: "Comment not found" });
 
-    if (comment.user.toString() !== req.user._id.toString() && comment.post.author.toString() !== req.user._id.toString()){
-        return res.status(401).json({message: "Not authorized to delete this comment"})
+    if (
+      comment.user.toString() !== req.user._id.toString() &&
+      comment.post.author.toString() !== req.user._id.toString()
+    ) {
+      return res
+        .status(401)
+        .json({ message: "Not authorized to delete this comment" });
     }
 
-    await comment.deleteOne()
-    res.json({message: "Comment deleted successfully"})
+    await comment.deleteOne();
+    res.json({ message: "Comment deleted successfully" });
   } catch (error) {
-    res.status(500).json({message: "Error deleting comment"})
+    res.status(500).json({ message: "Error deleting comment" });
   }
 };
--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const {createComment, updateComment, deleteComment} = require('../controllers/commentController')
+const {getComments, createComment, updateComment, deleteComment} = require('../controllers/commentController')
 
 const protect = require('../middleware/authMiddleware')
 
+router.get('/posts/:postId/comments', getComments)
 router.post('/:postId', protect, createComment)
 router.put('/:id', protect, updateComment)
 router.delete('/:id', protect, deleteComment)
