27ec5af428a92a1533014164ce5fc3bad021d53a
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-07-29 18:59:06 +0300

    Install fs and setup cloudinary

---
+++ backend/utils/cloudinary.js
@@ -0,0 +1,22 @@
+const cloudinary = require('cloudinary')
+const fs =  require('fs')
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_CLOUD_KEY,
+    api_secret: process.env.CLOUDINARY_CLOUD_SECRET,
+})
+
+const uploadToCloudinary = async (filePath) => {
+    try {
+        const result = await cloudinary.uploader.upload(filePath, {
+            folder: "blog-app",
+        })
+        fs.unlinkSync(filePath)
+        return result
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = { uploadToCloudinary}
