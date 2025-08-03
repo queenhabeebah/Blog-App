18d4c04b24567823cc5971b5b038eee99912eae6
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-08-03 23:21:51 +0300

    Update backend with cloud api info

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -3,8 +3,8 @@ const fs =  require('fs')
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_CLOUD_KEY,
-    api_secret: process.env.CLOUDINARY_CLOUD_SECRET,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
 const uploadToCloudinary = async (filePath) => {
