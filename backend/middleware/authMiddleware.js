a77085eeefe567928eb022748d60c725ce857de5
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-10-28 22:40:44 +0300

    Fix uploads directory address in multer

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -8,7 +8,7 @@ const protect = async (req, res, next) => {
         req.headers.authorization && req.headers.authorization.startsWith('Bearer')
     ){
         try{
-            token = req.headers.authorization.split(' ')[1]
+            token = req.headers.authorization?.split(' ')[1]
 
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
